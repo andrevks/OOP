43c76bc9dc415a3c7744d7dcf6d6cb5d2ceeae9c
andrevks <italiabk@gmail.com>
2020-01-14 17:10:17 -0400

    Inheritance of multiple classes

---
+++ Aula48_HerançaMúltipla/classes.h
@@ -0,0 +1,28 @@
+#ifndef CLASSES_H_INCLUDED
+#define CLASSES_H_INCLUDED
+
+class Base1{
+public:
+	void impBase1();
+};
+
+void Base1::impBase1(){
+	std::cout<<"Imp classe Base1"<<std::endl;
+}
+//***************
+class Base2{
+public:
+	void impBase2();
+};
+
+void Base2::impBase2(){
+	std::cout <<"Imp classe Base2"<<std::endl;
+}
+
+//Aqui é feito a herança múltipla, então:
+class CFB:public Base1,public Base2{
+
+};
+
+
+#endif
